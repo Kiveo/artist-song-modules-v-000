d57e19f00b60e9b4e0412bd22a7c266399785d19
Maverick Tesini <mavericktesini@gmail.com>
2018-03-22 19:11:45 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,18 @@
 module Memorable
+  module ClassMethods
+    def count
+      self.all.count
+    end
 
-def count
-  self.all.count
-end
+    def reset_all
+      self.all.clear
+    end
+  end #end of ClassMethods
 
-def reset_all
-  self.all.clear
-end
+  module InstanceMethods
 
-end
+    def initialize
+      self.class.all << self
+    end
+
+end #end of class Memorable
